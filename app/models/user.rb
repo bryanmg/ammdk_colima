86d038e7da7e8b95f73704242d9d83af075f94c5
bryan <bmendoza5@ucol.mx>
2022-01-18 13:48:37 -0600

    Run rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   belts = {
-    kup_10: "10° Kup", kup_9: "9° Kup", kup_8: "8° Kup", kup_7: "7° Kup", kup_6: "6° Kup", kup_5: "5° Kup",
-    kup_4: "4° Kup", kup_3: "3° Kup", kup_2: "2° Kup", kup_1: "1° Kup", kup_y: "1° Kup Yebidan",
-    dan_1: "1° Dan", dan_2: "2° Dan", dan_3: "3° Dan", dan_4: "4° Dan", dan_5: "5° Dan", dan_5: "5° Dan",
-    dan_7: "7° Dan", dan_8: "8° Dan", dan_9: "9° Dan"
+    kup10: "10° Kup", kup9: "9° Kup", kup8: "8° Kup", kup7: "7° Kup", kup6: "6° Kup", kup5: "5° Kup",
+    kup4: "4° Kup", kup3: "3° Kup", kup2: "2° Kup", kup1: "1° Kup", kup_y: "1° Kup Yebidan",
+    dan1: "1° Dan", dan2: "2° Dan", dan3: "3° Dan", dan4: "4° Dan", dan5: "5° Dan", dan6: "6° Dan",
+    dan7: "7° Dan", dan8: "8° Dan", dan9: "9° Dan"
   }
-  
-  enum :role, {sabonim: "sabonim", student: "student"}, scopes: false, default: "student"
+
+  enum :role, { sabonim: "sabonim", student: "student" }, scopes: false, default: "student"
   enum :belt, belts, scopes: false, default: nil
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,36 +2,36 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create([
-    {
-        email: "admin@gmail.com",
-        password: "123456",
-        name: "Jhon Doe",
-        role: "sabonim",
-        belt: "dan_4",
-        birth_date: "12/12/1995",
-        created_at: DateTime.new,
-        updated_at: DateTime.new
-    },
-    {
-        email: "student1@gmail.com",
-        password: "123456",
-        name: "Student 1",
-        role: "student",
-        belt: "kup_3",
-        birth_date: "12/12/2000",
-        created_at: DateTime.new,
-        updated_at: DateTime.new
-    },
-    {
-        email: "student2@gmail.com",
-        password: "123456",
-        name: "Student 2",
-        role: "student",
-        belt: "kup_2",
-        birth_date: "12/12/2010",
-        created_at: DateTime.new,
-        updated_at: DateTime.new
-    }   
-])
+              {
+                email: "admin@gmail.com",
+                password: "123456",
+                name: "Jhon Doe",
+                role: "sabonim",
+                belt: "dan_4",
+                birth_date: "12/12/1995",
+                created_at: DateTime.new,
+                updated_at: DateTime.new
+              },
+              {
+                email: "student1@gmail.com",
+                password: "123456",
+                name: "Student 1",
+                role: "student",
+                belt: "kup_3",
+                birth_date: "12/12/2000",
+                created_at: DateTime.new,
+                updated_at: DateTime.new
+              },
+              {
+                email: "student2@gmail.com",
+                password: "123456",
+                name: "Student 2",
+                role: "student",
+                belt: "kup_2",
+                birth_date: "12/12/2010",
+                created_at: DateTime.new,
+                updated_at: DateTime.new
+              }
+            ])
 
 p "Seed... Created #{User.count} users"
