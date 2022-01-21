bc007b3ad6c7b14839226bfc78f16a3e7487ce77
bryan <bmendoza5@ucol.mx>
2022-01-21 13:47:35 -0600

    Fix rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root "dashboard#index"
   get "/", to: "dashboard#index"
-  
+
   resources :students
   resources :teachers
   resources :documents
