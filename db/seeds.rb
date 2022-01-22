b934f9df2b42f008bedded0f42d078ad610c153c
bryan <bmendoza5@ucol.mx>
2022-01-22 00:01:36 -0600

    Replace seeds create for insert_all fonction for speed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,34 @@
-# Examples:
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-User.create([
-              {
-                email: "admin@gmail.com",
-                password: "123456",
-                name: "Jhon Doe",
-                role: "sabonim",
-                belt: "dan_4",
-                birth_date: "12/12/1995",
-                created_at: DateTime.new,
-                updated_at: DateTime.new
-              },
-              {
-                email: "student1@gmail.com",
-                password: "123456",
-                name: "Student 1",
-                role: "student",
-                belt: "kup_3",
-                birth_date: "12/12/2000",
-                created_at: DateTime.new,
-                updated_at: DateTime.new
-              },
-              {
-                email: "student2@gmail.com",
-                password: "123456",
-                name: "Student 2",
-                role: "student",
-                belt: "kup_2",
-                birth_date: "12/12/2010",
-                created_at: DateTime.new,
-                updated_at: DateTime.new
-              }
-            ])
+User.insert_all([
+                  {
+                    email: "admin@gmail.com",
+                    encrypted_password: "123456",
+                    name: "Jhon Doe",
+                    role: "sabonim",
+                    belt: :dan4,
+                    birth_date: "12/12/1995",
+                    created_at: DateTime.new,
+                    updated_at: DateTime.new
+                  },
+                  {
+                    email: "student1@gmail.com",
+                    encrypted_password: "123456",
+                    name: "Student 1",
+                    role: "student",
+                    belt: :kup10,
+                    birth_date: "12/12/2000",
+                    created_at: DateTime.new,
+                    updated_at: DateTime.new
+                  },
+                  {
+                    email: "student2@gmail.com",
+                    encrypted_password: "123456",
+                    name: "Student 2",
+                    role: "student",
+                    belt: :kup8,
+                    birth_date: "12/12/2010",
+                    created_at: DateTime.new,
+                    updated_at: DateTime.new
+                  }
+                ])
 
 p "Seed... Created #{User.count} users"
