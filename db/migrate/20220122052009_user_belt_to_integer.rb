87d2829820f49739397808ad4bec890bdac8156c
bryan <bmendoza5@ucol.mx>
2022-01-23 12:59:21 -0600

    Fix unnecessary migration for modify belt

--- db/migrate/20220122052009_user_belt_to_integer.rb
+++
@@ -1,7 +0,0 @@
-class UserBeltToInteger < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :belt
-    add_column :users, :belt, :integer
-    add_index :users, :belt
-  end
-end
--- db/migrate/20220123185631_add_initial_user_information.rb
+++ db/migrate/20220123185631_add_initial_user_information.rb
@@ -1,8 +1,8 @@
-class AddNameToUser < ActiveRecord::Migration[7.0]
+class AddInitialUserInformation < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
     add_column :users, :role, :integer
-    add_column :users, :belt, :string
+    add_column :users, :belt, :integer
     add_column :users, :birth_date, :date
 
     add_index :users, :belt
