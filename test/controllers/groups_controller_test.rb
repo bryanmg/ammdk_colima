c2a6d2876c9819f9396a19cf7e5681daf6c3e05a
bryan <bmendoza5@ucol.mx>
2022-01-24 11:44:01 -0600

    Update group controller test

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -3,17 +3,21 @@ require "test_helper"
 class GroupsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
-    @group = groups(:one)
+    @groupsss = groups(:one)
+    @user.groups.build(
+      { from_time: @groupsss.from_time, name: @groupsss.name, to_time: @groupsss.to_time }
+    ).save
+    @group = @user.groups.last
   end
 
   test "should get index" do
-    get user_groups_url(user_id: @user.id)
+    get user_groups_url(@user)
 
     assert_response :success
   end
 
   test "should get new" do
-    get new_user_group_url(user_id: @user.id)
+    get new_user_group_url(@user)
 
     assert_response :success
   end
@@ -21,43 +25,42 @@ class GroupsControllerTest < ActionDispatch::IntegrationTest
   test "should create group" do
     assert_difference("Group.count") do
       params = {
-        user_id: @user.id,
-        group: { from_time: @group.from_time, name: @group.name, to_time: @group.to_time }
+        group: {
+          from_time: @group.from_time, name: @group.name, to_time: @group.to_time
+        }
       }
-      post user_groups_url(params)
+      post user_groups_url(@user), params: params
     end
 
-    assert_redirected_to user_groups_url(user_id: @user.id, id: Group.last.id)
+    assert_redirected_to user_groups_url(@user, Group.last)
   end
 
   test "should show group" do
-    get user_groups_url(user_id: @user.id, id: @group.id)
+    get user_groups_url(@user, @group)
 
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_user_group_url(user_id: @user.id, id: @group.id)
+    get edit_user_group_url(@user, @group)
 
     assert_response :success
   end
 
   test "should update group" do
     params = {
-      user_id: @user.id,
-      id: @group.id,
       group: { from_time: @group.from_time, name: @group.name, to_time: @group.to_time }
     }
-    patch user_group_url(params)
+    patch user_group_url(@user, @group), params: params
 
-    assert_redirected_to user_groups_url(user_id: @user.id, id: @group.id)
+    assert_redirected_to user_groups_url(@user, @group)
   end
 
   test "should destroy group" do
     assert_difference("Group.count", -1) do
-      delete user_group_url(user_id: @user.id, id: @group.id)
+      delete user_group_url(@user, @group)
     end
 
-    assert_redirected_to user_groups_url(user_id: @user.id)
+    assert_redirected_to user_groups_url(@user)
   end
 end
