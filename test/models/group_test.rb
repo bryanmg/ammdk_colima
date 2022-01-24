2810d438b12b619a78b1499b375a9328a20c3aa5
bryan <bmendoza5@ucol.mx>
2022-01-23 22:19:38 -0600

    Fix prettier errors

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -4,12 +4,14 @@ class GroupTest < ActiveSupport::TestCase
   test "is not valid without from time" do
     group = Group.new
     refute group.valid?
+
     assert_includes group.errors.messages[:from_time], "can't be blank"
   end
 
   test "is not valid without to time" do
     group = Group.new
     refute group.valid?
+
     assert_includes group.errors.messages[:to_time], "can't be blank"
   end
 end
