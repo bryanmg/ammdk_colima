465eac3c76f4c7304bca55fc6414fc9f59237be0
bryan <bmendoza5@ucol.mx>
2022-01-23 22:20:15 -0600

    Add database validations to groups

--- db/migrate/20220118183613_create_groups.rb
+++ db/migrate/20220118183613_create_groups.rb
@@ -1,9 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.string :name
-      t.time :from_time
-      t.time :to_time
+      t.string :name, null: false
+      t.time :from_time, null: false
+      t.time :to_time, null: false
 
       t.timestamps
     end
