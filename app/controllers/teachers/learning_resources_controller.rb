9255bfdf9e4d8dcf8d94a47262ea5373d8f918b7
bryan <bmendoza5@ucol.mx>
2022-02-01 10:22:46 -0600

    Apply rubocop

--- app/controllers/teachers/learning_resources_controller.rb
+++ app/controllers/teachers/learning_resources_controller.rb
@@ -1,46 +1,46 @@
-class Teachers::LearningResourcesController < ApplicationController
-  before_action :authenticate_user!, :set_user
-  before_action :set_teachers_learning_resource, only: %i[ show edit update destroy ]
+module Teachers
+  class LearningResourcesController < ApplicationController
+    before_action :authenticate_user!, :set_user
+    before_action :set_teachers_learning_resource, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @teacher_learning_resources = @user.learning_resources
-  end
-
-  def show
-  end
-
-  def new
-    @teachers_learning_resource = @user.learning_resources.new
-  end
-
-  def edit
-  end
-
-  def create
-    @teachers_learning_resource = @user.learning_resources.new(learning_resource_params)
-
-    if @teachers_learning_resource.save
-      redirect_to teacher_learning_resources_url(@user), notice: "Learning resource was successfully created."
-    else
-      render :new, status: :unprocessable_entity
+    def index
+      @teacher_learning_resources = @user.learning_resources
     end
-  end
 
-  def update
-    if @teachers_learning_resource.update(learning_resource_params)
-      redirect_to teacher_learning_resources_url(@teachers_learning_resource), notice: "Learning resource was successfully updated."
-    else
-      render :edit, status: :unprocessable_entity
+    def show; end
+
+    def new
+      @teachers_learning_resource = @user.learning_resources.new
     end
-  end
 
-  def destroy
-    @teachers_learning_resource.destroy
+    def edit; end
 
-    redirect_to teacher_learning_resources_url(@user), notice: "Learning resource was successfully destroyed."
-  end
+    def create
+      @teachers_learning_resource = @user.learning_resources.new(learning_resource_params)
 
-  private
+      if @teachers_learning_resource.save
+        redirect_to teacher_learning_resources_url(@user), notice: "Learning resource was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def update
+      if @teachers_learning_resource.update(learning_resource_params)
+        redirect_to teacher_learning_resources_url(@teachers_learning_resource),
+                    notice: "Learning resource was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @teachers_learning_resource.destroy
+
+      redirect_to teacher_learning_resources_url(@user), notice: "Learning resource was successfully destroyed."
+    end
+
+    private
 
     def set_user
       user_id = current_user.id || params[:id]
@@ -54,4 +54,5 @@ class Teachers::LearningResourcesController < ApplicationController
     def learning_resource_params
       params.require(:learning_resource).permit(:name, :description, :belt, :resource)
     end
+  end
 end
