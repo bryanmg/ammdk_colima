59934fa3257891201d2008647c489d0987d70d95
bryan <bmendoza5@ucol.mx>
2022-02-10 19:30:09 -0600

    Revert "Configure custom js"
    This reverts commit 6ebe46bc53e302bb5d7bfaac4e3ae97fea84351b.

--- app/javascript/application.js
+++
@@ -1,5 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
-//import "paginator"
-import "custom/example"
-import "@hotwired/turbo-rails"
