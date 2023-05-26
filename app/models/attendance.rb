2ac5199e29537ede100da5b2c2d64c2af3562c5c
Bryan Mendoza Guerrero <bryan.mendoza.michelada@kueski.com>
2023-05-26 16:49:02 -0600

    Fix can update attendances of the same day

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -7,4 +7,18 @@ class Attendance < ApplicationRecord
   def attendance_recod_for
     group_by(&:user_id)
   end
+
+  def create_or_update_for(attendances)
+    create_or_update(attendances)
+  end
+
+  def self.upsert!(args)
+    args.each do |att|
+      is = att.delete(:present)
+
+      return where(att).update(att.merge({ present: is })) if exists?(att)
+
+      create!(att.merge({ present: is }))
+    end
+  end
 end
